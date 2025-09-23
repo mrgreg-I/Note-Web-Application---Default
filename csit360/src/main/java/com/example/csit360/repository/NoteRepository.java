ba1688d6605bdf5dd2cfa27ca3643a1fac67853c
DymurBoi <dymurboi@gmail.com>
2025-09-23 12:05:45 +0800

    backend

---
+++ csit360/src/main/java/com/example/csit360/repository/NoteRepository.java
@@ -0,0 +1,12 @@
+package com.example.csit360.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.csit360.entity.Note;
+
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Integer>{
+    public Note findByNoteId(int noteId);
+}
