d09db7a3b4828bcb973f493f9f9a30cec6cd71ce
Virtue <dymurboi@gmail.com>
2025-12-10 09:57:23 +0800

    Routes fix

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,8 +1,6 @@
 // src/RoutesConfig.jsx
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import Register from './components/Register';
-import Login from './components/Login';
 import TaskView from './components/TaskView';
 import TaskUpdate from './components/TaskUpdate';
 import TaskCreate from './components/TaskCreate';
@@ -11,10 +9,8 @@ import BlockchainDashboard from './components/BlockchainDashboard';
 
 const RoutesConfig = ({ loggedInUserId, handleLogin }) => (
   <Routes>
-  <Route path="/" element={<Navigate to="/register" />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/login" element={<Login onLogin={handleLogin} />} />
-    <Route path="/tasks" element={<TaskView userId={loggedInUserId} />} />
+  <Route path="/" element={<Navigate to="/tasks" />} />
+    <Route path="/tasks" element={<TaskView/>} />
     <Route path="/blockchain" element={<BlockchainDashboard userId={loggedInUserId} />} />
     {/*<Route path="/taskview/:toDoListID" element={<TaskView />} /> */}
     <Route path="/taskupdate/:taskId" element={<TaskUpdate/>}/>
